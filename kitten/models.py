e1d5351a3ee992ce8a46a745d17a36e2f320d75d
Mbarakaj4 <113691935+Mbarakaj4@users.noreply.github.com>
2023-10-10 04:13:28 -0300

    Fix: models.py
    Fixed the User model

--- kitten/models.py
+++ kitten/models.py
@@ -8,6 +8,9 @@ class User(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email_address = models.CharField(max_length=255)
-    age = models.IntegerField()
+    password = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} {self.email_address} {self.created_at} {self.updated_at}"
