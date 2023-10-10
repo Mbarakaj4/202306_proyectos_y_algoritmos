ea7ae1e43d53d2dfd1d81d1ccdc999ad9643b0ec
Mbarakaj4 <113691935+Mbarakaj4@users.noreply.github.com>
2023-10-10 04:15:48 -0300

    Update: views.py
    Update the data handling and add some error handling structures

--- kitten/views.py
+++ kitten/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import redirect, render
+from django.db.models import Q
+from django.contrib.auth.hashers import make_password, check_password
+from .models import User
 
 # Create your views here.
 
@@ -15,10 +18,20 @@ def auth(request):
     if request.method == "POST":
         email = request.POST.get("email")
         password = request.POST.get("password")
-        request.session['user'] = email
-        print(email, password)
-        return redirect("/dashboard/")
-
+        data = {
+            "email": email,
+            "password": password
+        }
+        # print(email, password)
+        try:
+            user = User.objects.filter( Q(email_address__startswith=data['email']) )
+        except:
+            print("User not found")
+            return redirect("/")
+        else:
+            request.session['user'] = data["email"]
+            if check_password(data['password'], user[0].password):
+                return redirect("/dashboard/")
 
 # Register function
 def register(request):
@@ -27,9 +40,27 @@ def register(request):
         last_name = request.POST.get("last_name")
         email = request.POST.get("email")
         password = request.POST.get("password")
-        request.session['user'] = email
-        print(email, password, first_name, last_name)
-        return redirect("/dashboard/")
+        data = {
+            "first_name": first_name,
+            "last_name": last_name,
+            "email": email,
+            "password": password,
+        }
+        # print(email, password, first_name, last_name)
+        try:
+            user = User(
+                first_name=data['first_name'], 
+                last_name=data['last_name'], 
+                email_address=data['email'], 
+                password=make_password(data['password'])
+            )
+        except:
+            print('Something went wrong with the registration')
+            return redirect("/")
+        else:
+            user.save()
+            request.session['user'] = email
+            return redirect("/dashboard/")
     
 
 # Logout function
@@ -41,7 +72,11 @@ def logout(request):
 # Dashboard view
 def dashboard(request):
     if 'user' in request.session:
-        return render(request, "kitten/index.html")
+        users = User.objects.all()
+        data = {
+            "users": users
+        }
+        return render(request, "kitten/index.html", data)
     else:
         return redirect("/")
 
