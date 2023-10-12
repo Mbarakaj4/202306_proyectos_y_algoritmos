abf0b04486f890d1c7ba1a7f85f1be45e0a1e612
Mbarakaj4 <113691935+Mbarakaj4@users.noreply.github.com>
2023-10-12 16:06:32 -0300

    Update models.py

--- kitten/models.py
+++ kitten/models.py
@@ -5,12 +5,13 @@ from django.db import models
 # Create User model using django ORM
 class User(models.Model):
 
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-    email_address = models.CharField(max_length=255)
-    password = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    first_name      = models.CharField(max_length=255)
+    last_name       = models.CharField(max_length=255)
+    email_address   = models.CharField(max_length=255)
+    password        = models.CharField(max_length=255)
+    phone_number    = models.CharField(max_length=255, blank=True, null=True)
+    created_at      = models.DateTimeField(auto_now_add=True)
+    updated_at      = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name} {self.email_address} {self.created_at} {self.updated_at}"
+        return f"{self.first_name} {self.last_name} {self.email_address} {self.phone_number} {self.created_at} {self.updated_at}"
